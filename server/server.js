69c9e35f3a779183be40b690e2c6111249d1774b
Satish Hiremath <satihir1@in.ibm.com>
2020-09-08 19:44:49 +0530

    Query Processing using PubSub

--- server/server.js
+++ server/server.js
@@ -1,109 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
-// const PORT = 3500;
-
-const authors = [
-    {   id:"1",
-        info: {
-            name:"Joe kelly",
-            age:32,
-            gender:"M",
-        }
-    },
-    {
-        id:"2",
-        info:{
-            name:"Mary Jame",
-            age:25,
-            gender:"F",
-        }
-    }
-];
-
-//The graphql schema in string form
-const typeDefs = `
-type Author {
-    id: ID!
-    info:Person
-}
-type Person {
-    name: String!
-    age:Int
-    gender:String
-}
-type Query {
-    getAuthors: [ Author ]
-    retrieveAuthor(id: ID!):Author 
-}
-type DeleteMessage{
-    id:ID!
-    message:String
-}
-type Mutation{
-    createAuthor(name: String!, gender: String!, age: Int):Author
-    updateAuthor(id:ID!, name: String, gender: String, age: Int):Author
-    deleteAuthor(id: ID!):DeleteMessage
-}
-`;
+const typeDefs = require('./schema');
+const resolvers = require('./resolver')
+const { createServer } = require('http');
 
 const PORT = 3600;
-
-//The Resolvers
-const resolvers = {
-    Query : {
-        getAuthors: () => authors,
-        retrieveAuthor: (obj, { id }) => authors.find(author => author.id === id),      
-    },
-    Mutation: {
-        createAuthor:(obj, args) => {
-            const id = String(authors.length+1);
-            const { name, gender, age } = args;
-
-            const newAuthor = {
-                id,
-                info: {
-                    name,
-                    gender,
-                    age
-                }
-            }
-            authors.push(newAuthor);
-            return newAuthor;
-        },
-        updateAuthor: (obj, {id, name, gender, age}) => {
-            const author = authors.find(author => author.id === id);
-            if(author) {
-                const authorIndex = authors.indexOf(author);
-
-                if(name) author.info.name = name;
-            
-                if(name)  author.info.age = age;
-                
-                if(name) author.info.gender = gender;
-
-                authors[authorIndex] = {id, info: author};  
-                return {id, info: author}
-            }else {
-                throw new Error("Author ID not found");
-            }
-        },
-        deleteAuthor: (obj, {id}) => {
-            const author = authors.find(author => author.id === id);
-            if(author) {
-                const authorIndex = authors.indexOf(author);
-                authors.splice(authorIndex, 1);
-                return {
-                    id, 
-                    message:`Author with id ${id} deleted successfully.`
-                };
-            }
-            else {
-                throw new Error("Author ID not found");
-            }
-        }
-    }
-};
 
 //Put together a schema
 const server = new ApolloServer({typeDefs, resolvers});
@@ -115,11 +16,14 @@ server.applyMiddleware({
     path: '/graphql'
 })
 
+const httpServer = createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
 app.use('/graphql', (req,res) => {
     res.send('Welcome to Graphql Applicaton');
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log('Server ready at port http://localhost:${PORT}${server.graphqlPath}');
 });
 
